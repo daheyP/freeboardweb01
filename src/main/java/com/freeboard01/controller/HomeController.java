dfe22467aa97e6bee005c54ae54f1b330d2b5184
daheyP <wlswnthsuv@naver.com>
2021-12-05 02:21:15 +0900

    Initial commit

---
+++ src/main/java/com/freeboard01/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.freeboard01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String home(Model model){
+        model.addAttribute("msg", "Hello World!");
+        return "index";
+    }
+
+    @GetMapping("/anotherpage")
+    public ModelAndView buttonTest(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("msg", "다른 페이지로 이동하였습니다!!!");
+        return modelAndView;
+    }
+
+}
